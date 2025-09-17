15feaf69164d20cb30f168a0de63501b0fd22794
SandraPautonnier <sandrapautonnier@gmail.com>
2025-09-17 16:22:01 +0200

    update backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,43 @@
 const express = require('express');
-const path = require('path');
-const cors = require('cors')
-require('dotenv').config();
+const cors = require('cors');
 const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-      crossOriginResourcePolicy: false,
-    }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 
+const app = express();
+
+// Configurer CORS pour autoriser uniquement ton frontend GitHub Pages
+app.use(cors({
+  origin: 'https://sandrapautonnier.github.io/Portfolio-architecte-sophie-bluel-master/', // <-- Remplace par l'URL de ton frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Méthodes autorisées
+  credentials: true // si tu utilises les cookies ou l'authentification
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet({
+  crossOriginResourcePolicy: false,
+}));
+
+// Fichiers statiques
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Base de données et routes
 const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
+
+db.sequelize.sync().then(() => console.log('db is ready'));
+
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 module.exports = app;
+
